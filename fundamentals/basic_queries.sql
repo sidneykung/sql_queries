70c9d051c0358c86b9104446822d38f2bd9373ca
Sidney Kung <sidney8899@gmail.com>
2021-02-15 14:57:29 -0500

    minor edit

--- fundamentals/basic_queries.sql
+++ fundamentals/basic_queries.sql
@@ -1,1 +1,17 @@
 -- this is a file for basic queries
+
+-- database organization
+
+-- SELECT
+
+-- selecting rows
+
+-- selecting columns
+
+-- counting rows
+
+-- inserting data
+
+-- updating data
+
+-- deleting data
